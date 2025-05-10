9d8cfae395df984b343d3d855d24dce0890003a0
SHIVAMGURJAR0000 <dheerajgurjar55556@gmail.com>
2025-05-11 01:07:42 +0530

    query parameter

--- functions/api.js
+++ functions/api.js
@@ -4,19 +4,19 @@ import ServerlessHttp from "serverless-http";
 const app = express();
 
 app.use(express.json());
-app.get("/.netlify/functions/api", (req, res) => {
+app.get("/.netlify/functions/api/", (req, res) => {
   res.json({
     message: "hello",
   });
 });
 
 app.post("/.netlify/functions/api/userArr", (req, res) => {
-  const { data } = req.body;
+  const ids = req.query.ids; // "1,2,3,4"
+  const idArray = ids ? ids.split(",") : [];
 
-  console.log(data);
   res.json({
     message: "Data Received",
-    data: data,
+    data: idArray,
   });
 });
 
